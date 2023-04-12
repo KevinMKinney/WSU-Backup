e7071b96d4cd240dd2f7cd4d43d77364184f0612
Kevin <kevin.kinney@wsu.edu>
2023-04-11 17:13:00 -0700

    Now checks if the backup directory exists and makes one if it does not.

--- BackItUp.c
+++ BackItUp.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 #include <pthread.h>
 #include <unistd.h>
-#include <sys/time.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#define BACKUPDIRNAME "backup"
 
 int main(int argc, char const *argv[]) {
-    printf("'Back It Up'ing...\n");
+    
+    DIR* backDir = opendir(BACKUPDIRNAME);
+
+    if (backDir == NULL) {
+        if (errno != ENOENT) {
+            perror(strerror(errno));
+            exit(1);
+        }
+
+        // could not find directory, so make a new one
+        mkdir(BACKUPDIRNAME, S_IRWXU);
+    }
+
+    
     return 0;
 }
